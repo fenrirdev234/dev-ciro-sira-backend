30fd76a4e5480186149e9f21329e589e7cf15fef
Ciro S <ciroalejandro1@gmail.com>
2025-02-21 11:23:30 -0400

    feat: add basic middleware

---
+++ src/middleware/errorHandle.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (
+  error: any,
+  req: Request,
+  res: any,
+  next: NextFunction
+) => {
+  console.log(error);
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  /*  else {
+    // send generic error message
+    return res.status(error.status).json({
+      status: 'error',
+      message: 'Something went wrong'
+      })} */
+
+  next(error);
+};
