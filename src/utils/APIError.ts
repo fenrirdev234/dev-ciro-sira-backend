4e9d532665ebbebde3c87516decc58a74f6b32cd
Ciro S <ciroalejandro1@gmail.com>
2025-04-03 16:14:40 -0400

    feat: add APIError class

---
+++ src/utils/APIError.ts
@@ -0,0 +1,21 @@
+export class APIError extends Error {
+  status: number;
+  title: string;
+  message: string;
+  constructor(status = 500, title = "Internal Server Error", message = "An unknown server error occurred.") {
+    super(message);
+    this.status = status;
+    this.title = title;
+    this.message = message;
+  }
+  toJSON() {
+    const { status, title, message } = this;
+    return {
+      error: {
+        status,
+        title,
+        message,
+      },
+    };
+  }
+}
