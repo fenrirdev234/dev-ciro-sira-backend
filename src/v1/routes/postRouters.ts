6776ae5c47688a29a04340a599dde156a6838129
Ciro S <ciroalejandro1@gmail.com>
2025-04-17 01:13:11 -0400

    style: formmater post router

--- src/v1/routes/postRouters.ts
+++ src/v1/routes/postRouters.ts
@@ -1,30 +1,13 @@
 import { Router } from "express";
 
-import {
-  createPostsController,
-  getAllPostsController,
-  getOnePostsController,
-} from "../../controllers/postController";
+import { createPostsController, getAllPostsController, getOnePostsController } from "../../controllers/postController";
 import { multerUpload } from "../../middlewares/multerUpload";
 import { schemaValition } from "../../middlewares/schemaValidator";
-import {
-  CreatePostSchema,
-  GetAllPostSchema,
-  GetOnePostSchema,
-} from "../../schemas/postSchema";
+import { CreatePostSchema, GetAllPostSchema, GetOnePostSchema } from "../../schemas/postSchema";
 export const v1PostRouter = Router();
 
 v1PostRouter.get("/", schemaValition(GetAllPostSchema), getAllPostsController);
 
-v1PostRouter.get(
-  "/:postId",
-  schemaValition(GetOnePostSchema),
-  getOnePostsController
-);
+v1PostRouter.get("/:postId", schemaValition(GetOnePostSchema), getOnePostsController);
 
-v1PostRouter.post(
-  "/",
-  multerUpload.single("image"),
-  schemaValition(CreatePostSchema),
-  createPostsController
-);
+v1PostRouter.post("/", multerUpload.single("image"), schemaValition(CreatePostSchema), createPostsController);
