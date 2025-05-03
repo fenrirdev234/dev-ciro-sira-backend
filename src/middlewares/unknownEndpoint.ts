94cc81628ed00ca0f876df34f77151f69c4cdf6a
Ciro S <ciroalejandro1@gmail.com>
2025-05-03 00:29:45 -0400

    feat: todo error

--- src/middlewares/unknownEndpoint.ts
+++ src/middlewares/unknownEndpoint.ts
@@ -3,6 +3,8 @@ import type { Request, Response } from "express";
 
 import { APIError } from "../utils/APIError";
 export const unknownEndpoint = (request: Request, response: Response) => {
+  /* TODO: change Boom to  http-status-codes */
+
   const errorBoom = notFound("unknown endpoint");
 
   const errorFormmater = new APIError(errorBoom.output.payload.statusCode, errorBoom.output.payload.error, errorBoom.output.payload.message);
